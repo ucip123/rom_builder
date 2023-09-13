601cb8b7aa95dc0cd83e75639beed73f629bd56b
ucip123 <98519520+ucip123@users.noreply.github.com>
2023-09-12 22:45:23 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=ucip123
+GITHUB_EMAIL=oppo765qq@gmail.com
 
-KBUILD_BUILD_USER="Aki"
-KBUILD_BUILD_HOST="A_DEAD_PLANET"
+KBUILD_BUILD_USER="ucipo"
+KBUILD_BUILD_HOST="DED"
 
-export oem=motorola
-export device=harpia
+export oem=realme
+export device=even
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+branch="eleven-plus"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="ucip123/rom_builder"
 
-timezone="Asia/Kolkata"
+timezone="Asia/Singapore"
