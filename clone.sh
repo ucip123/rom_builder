d208686acf35aacd5b4f49e2add3af7c5723c4cc
ucip123 <98519520+ucip123@users.noreply.github.com>
2023-09-12 22:56:53 -0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-# write commands if you want to clone some repos
+# clone repos PE cuma nyobah - even
+
+GITHUB='https://github.com/ghostrider-reborn'
+BRANCH="eleven-plus"
+
+git clone https://github.com/KharaMe-devices/vendor_realme_even vendor/realme/even
+git clone https://github.com/KharaMe-devices/device_realme_even -b eleven-rui2 device/realme/even
+git clone https://github.com/ucip123/liquid_kernel_realme_even -b OSS-RUI2-NO-KSU kernel/realme/even
